41334315cd02b3976d0744ec90deb3f2971d140d
Diego A. Andrade <d_4_@hotmail.com>
2026-01-23 16:32:46 -0600

    README update

--- scripts/run_anamacro_1gX_blips_signal_R70-115_optInputs.sh
+++ scripts/run_anamacro_1gX_blips_signal_R70-115_optInputs.sh
@@ -7,20 +7,20 @@
 
 ../build/anamacro_1gX_blips_signal $DATA_PROD/MCC9.10/wc_processed/BNB/MCC9.10_Run4b_v10_04_07_09_BNB_nu_overlay_surprise_reco2_hist.root Signal false false SIGNAL_CRT_R70_M10_E8 70.0 10 8.0
 
-#../build/anamacro_1gX_blips_signal $DATA_PROD/MCC9.10/wc_processed/BNB/MCC9.10_Run4b_v10_04_07_09_BNB_nu_overlay_surprise_reco2_hist.root Signal false false SIGNAL_CRT_R75_M10_E8 75.0 10 8.0
+../build/anamacro_1gX_blips_signal $DATA_PROD/MCC9.10/wc_processed/BNB/MCC9.10_Run4b_v10_04_07_09_BNB_nu_overlay_surprise_reco2_hist.root Signal false false SIGNAL_CRT_R75_M10_E8 75.0 10 8.0
 
-#../build/anamacro_1gX_blips_signal $DATA_PROD/MCC9.10/wc_processed/BNB/MCC9.10_Run4b_v10_04_07_09_BNB_nu_overlay_surprise_reco2_hist.root Signal false false SIGNAL_CRT_R80_M10_E8 80.0 10 8.0
+../build/anamacro_1gX_blips_signal $DATA_PROD/MCC9.10/wc_processed/BNB/MCC9.10_Run4b_v10_04_07_09_BNB_nu_overlay_surprise_reco2_hist.root Signal false false SIGNAL_CRT_R80_M10_E8 80.0 10 8.0
 
-#../build/anamacro_1gX_blips_signal $DATA_PROD/MCC9.10/wc_processed/BNB/MCC9.10_Run4b_v10_04_07_09_BNB_nu_overlay_surprise_reco2_hist.root Signal false false SIGNAL_CRT_R85_M10_E8 85.0 10 8.0
+../build/anamacro_1gX_blips_signal $DATA_PROD/MCC9.10/wc_processed/BNB/MCC9.10_Run4b_v10_04_07_09_BNB_nu_overlay_surprise_reco2_hist.root Signal false false SIGNAL_CRT_R85_M10_E8 85.0 10 8.0
 
-#../build/anamacro_1gX_blips_signal $DATA_PROD/MCC9.10/wc_processed/BNB/MCC9.10_Run4b_v10_04_07_09_BNB_nu_overlay_surprise_reco2_hist.root Signal false false SIGNAL_CRT_R90_M10_E8 90.0 10 8.0
+../build/anamacro_1gX_blips_signal $DATA_PROD/MCC9.10/wc_processed/BNB/MCC9.10_Run4b_v10_04_07_09_BNB_nu_overlay_surprise_reco2_hist.root Signal false false SIGNAL_CRT_R90_M10_E8 90.0 10 8.0
 
-#../build/anamacro_1gX_blips_signal $DATA_PROD/MCC9.10/wc_processed/BNB/MCC9.10_Run4b_v10_04_07_09_BNB_nu_overlay_surprise_reco2_hist.root Signal false false SIGNAL_CRT_R95_M10_E8 95.0 10 8.0
+../build/anamacro_1gX_blips_signal $DATA_PROD/MCC9.10/wc_processed/BNB/MCC9.10_Run4b_v10_04_07_09_BNB_nu_overlay_surprise_reco2_hist.root Signal false false SIGNAL_CRT_R95_M10_E8 95.0 10 8.0
 
-#../build/anamacro_1gX_blips_signal $DATA_PROD/MCC9.10/wc_processed/BNB/MCC9.10_Run4b_v10_04_07_09_BNB_nu_overlay_surprise_reco2_hist.root Signal false false SIGNAL_CRT_R100_M10_E8 100.0 10 8.0
+../build/anamacro_1gX_blips_signal $DATA_PROD/MCC9.10/wc_processed/BNB/MCC9.10_Run4b_v10_04_07_09_BNB_nu_overlay_surprise_reco2_hist.root Signal false false SIGNAL_CRT_R100_M10_E8 100.0 10 8.0
 
-#../build/anamacro_1gX_blips_signal $DATA_PROD/MCC9.10/wc_processed/BNB/MCC9.10_Run4b_v10_04_07_09_BNB_nu_overlay_surprise_reco2_hist.root Signal false false SIGNAL_CRT_R105_M10_E8 105.0 10 8.0
+../build/anamacro_1gX_blips_signal $DATA_PROD/MCC9.10/wc_processed/BNB/MCC9.10_Run4b_v10_04_07_09_BNB_nu_overlay_surprise_reco2_hist.root Signal false false SIGNAL_CRT_R105_M10_E8 105.0 10 8.0
 
-#../build/anamacro_1gX_blips_signal $DATA_PROD/MCC9.10/wc_processed/BNB/MCC9.10_Run4b_v10_04_07_09_BNB_nu_overlay_surprise_reco2_hist.root Signal false false SIGNAL_CRT_R110_M10_E8 110.0 10 8.0
+../build/anamacro_1gX_blips_signal $DATA_PROD/MCC9.10/wc_processed/BNB/MCC9.10_Run4b_v10_04_07_09_BNB_nu_overlay_surprise_reco2_hist.root Signal false false SIGNAL_CRT_R110_M10_E8 110.0 10 8.0
 
-#../build/anamacro_1gX_blips_signal $DATA_PROD/MCC9.10/wc_processed/BNB/MCC9.10_Run4b_v10_04_07_09_BNB_nu_overlay_surprise_reco2_hist.root Signal false false SIGNAL_CRT_R115_M10_E8 115.0 10 8.0
+../build/anamacro_1gX_blips_signal $DATA_PROD/MCC9.10/wc_processed/BNB/MCC9.10_Run4b_v10_04_07_09_BNB_nu_overlay_surprise_reco2_hist.root Signal false false SIGNAL_CRT_R115_M10_E8 115.0 10 8.0
